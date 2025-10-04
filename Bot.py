38a4fe918a2e4d5e492aa8ec22fc34eeb5a4b230
pogmaciek321 <maciekcz05@gmail.com>
2025-10-04 09:09:12 +0200

    dziala bot poprawnie
    odpowiada na komentarze z dog w nich na subreddicie testingground4bots

--- Bot.py
+++ Bot.py
@@ -1,5 +1,7 @@
 import praw
 import config
+import os
+import time
 def Reddit_login():
     log=praw.Reddit(username = config.username,
                 password = config.password,
@@ -8,17 +10,37 @@ def Reddit_login():
                 user_agent = "pogmaciek's bot test v0.1" )
     return log
 def Run_bot(log,comments_replied_to):
-    
-    for comment in log.subreddit('testofredditbot321').comments(limit=25):
-         if ("dog" in comment.body.lower()
+    my_name = str(log.user.me())
+    print("Bot username:", my_name)
+    comments = list(log.subreddit('testingground4bots').comments(limit=20))
+    print("Pobrano komentarzy:", len(comments))
+    for comment in comments:
+        print("Komentarz:", comment.body)
+        author_name = str(comment.author) if comment.author else ""
+        if ("dog" in comment.body.lower()
             and comment.id not in comments_replied_to
-            and comment.author != log.user.me()):
-            comment.reply("what the dog doin")
-            comments_replied_to.append(comment.id)
+            and author_name.lower() != my_name.lower()):
+                try:
+                    comment.reply("what the dog doin")
+                    print("comment found"+comment.id)
+                    comments_replied_to.append(comment.id)
+                    with open("comments_replied_to.txt","a")as f:
+                        f.write(comment.id+"\n")
+                    print(f"Zapisano do pliku: {comment.id}")
+                except Exception as e:
+                    print(f"nie udalo sie odpowiedziec")
         
-
+def read_comments_from_file():
+    if not os.path.isfile("comments_replied_to.txt"):
+        return []
+    else:
+        with open("comments_replied_to.txt", "r") as f:
+            comments_replied_to = [line.strip() for line in f if line.strip()]
+    return comments_replied_to
 
 log=Reddit_login()
-comments_replied_to = []
+comments_replied_to = read_comments_from_file()
 while True:
     Run_bot(log,comments_replied_to)
+    print("czekam troche")
+    time.sleep(30)
